6b6037a440d9656675afc6747b5a330ce90b2714
Hubert FONGARNAND <hubert@fongarnand.fr>
2013-10-27 20:34:04 +0100

    WIP

--- src/main/java/fr/esdeve/JPAListener.java
+++ src/main/java/fr/esdeve/JPAListener.java
@@ -19,7 +19,7 @@ public class JPAListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		// TODO Auto-generated method stub
-		LOG.info("Context destroyed...");
+		LOG.info("Context destroyed...2");
 		Enumeration<Driver> drivers = DriverManager.getDrivers();
         while (drivers.hasMoreElements()) {
             Driver driver = drivers.nextElement();
